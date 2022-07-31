5b9f543f539582479f9cdbb9d46a7b4de12fcdcf
WeatrixDeveloper <110183343+WeatrixDeveloper@users.noreply.github.com>
2022-07-31 03:01:12 -0700

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
+            content="How about moderating your own server with Lowzy bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
           />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@AstraBot" />
-          <meta name="twitter:creator" content="@AstraBot" />
-          <meta property="og:url" content="https://astrabot.vercel.app" />
+          <meta name="twitter:site" content="@LowzyBot" />
+          <meta name="twitter:creator" content="@LowzyBot" />
+          <meta property="og:url" content="https://lowzybot.tk/" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Astra Bot" />
+          <meta property="og:title" content="Lowzy Bot" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
+            content="How about moderating your own server with Lowzy bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="Astra Bot" />
+          <meta property="og:image:alt" content="Lowzy Bot" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="Astra Bot"
+            content="Lowzy Bot"
           />
           <meta name="theme-color" content="#6108dd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
